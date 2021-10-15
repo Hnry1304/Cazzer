492a0495adf5dce65e137d714d7c628baecba849
Hnry1304 <henryjvp24@hotmail.com>
2021-10-15 12:02:16 -0500

    Creamos datosLogin.php quien heredara sus datos a login.php

--- Models/Login/login.php
+++ Models/Login/login.php
@@ -1,31 +1,7 @@
 <?php
-    class login{
-
-        private $username;
-        private $password;
-        private $conexion;
-
-
-        public function __construct(){
-            $this->conexion = new Database();
-            $this->conexion = $this->conexion->connect();
-        }
-
-        public function setUsername($username){
-            $this->username = $username;
-        }
-
-        public function setPassword($password){
-            $this->password = $password;
-        }
-
-        public function getUsername(){
-            return $this->username;
-        }
-
-        public function getPassword(){
-            return $this->password;
-        }
+    require_once 'Models/Login/datosLogin.php';
+    
+    class login extends datosLogin{
 
         public function comprobar(){
 
