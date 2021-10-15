97d78a347bff777783dffa4ccf416c9850d116f1
Hnry1304 <henryjvp24@hotmail.com>
2021-10-15 03:08:56 -0500

    Usuario Existe pasamos al siguiente paso, no existe retornarnos a vista login

--- Controllers/loginController.php
+++ Controllers/loginController.php
@@ -1,8 +1,22 @@
 <?php
+    require_once 'Models/Login/login.php';
     class loginController{
 
         public function login(){
-            var_dump($_POST);
             require_once 'Validations/loginValidation.php';
+
+
+            $login = new Login();
+            $login->setUsername($username);
+            $login->setPassword($contrasena);
+
+            $resultado = $login->comprobar();
+            
+            if(!$resultado){
+                header('Location: index.php?class=views&function=viewLoginPage');
+
+            }else{
+                require_once 'Views/Admin/viewAdminPage.php';
+            }
         }
     }
