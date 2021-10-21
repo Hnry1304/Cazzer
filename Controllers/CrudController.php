1d68fca6f79766c3f2e269d71ca68aeb672f8272
Hnry1304 <henryjvp24@hotmail.com>
2021-10-21 11:25:05 -0500

    Modificamos el crud desde 0

--- Controllers/CrudController.php
+++ Controllers/CrudController.php
@@ -1,13 +1,26 @@
 <?php
+    require_once 'Models/Admin/Crud.php';
     class CrudController{
 
-        public function agregarImagen(){
+        public function addImagenes(){
 
-            print_r($_FILES);
+            $url = $_FILES['url']['name'];
+
             $carpeta_destino = 'imagenes/';
-            $archivo_subido = $carpeta_destino . $_FILES['Url']['name'];
-            move_uploaded_file($_FILES['Url']['tmp_name'], $archivo_subido);
+            $archivo_subido = $carpeta_destino . $_FILES['url']['name'];
+            move_uploaded_file($_FILES['url']['tmp_name'],$archivo_subido);
 
+            $add_imagenes = new Crud;
+            $add_imagenes->setUrl($url);
+            $add_imagenes->add();
 
+            header('Location: index.php?class=Views&function=viewAddImagenes');
+        }
+
+        public function readImagenes(){
+            $read_imagenes = new Crud;
+            $information = $read_imagenes->read();
+
+            require_once 'Views/Admin/viewHomePageAdmin.php';
         }
     }
