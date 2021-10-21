57a3585a8a3f3bda48045cafd397db628c3a4882
Hnry1304 <henryjvp24@hotmail.com>
2021-10-21 12:54:20 -0500

    Agregamos la ruta Css para imagenes

--- Views/Assets/Layouts/Admin/header.php
+++ Views/Assets/Layouts/Admin/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="Views/Assets/Css/header.css">
+    <link rel="stylesheet" href="Views/Assets/Css/imagenes.css">
 </head>
 <body>
     <div class="container">
