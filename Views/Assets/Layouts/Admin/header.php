96fb8c3bd08c558f42b0c2ed83c8d9f4c4933092
Hnry1304 <henryjvp24@hotmail.com>
2021-10-21 12:27:22 -0500

    Pequenos cambios a header De Admi

--- Views/Assets/Layouts/Admin/header.php
+++ Views/Assets/Layouts/Admin/header.php
@@ -5,13 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Views/Assets/Css/header.css">
 </head>
 <body>
     <div class="container">
-        <nav class="navegations">
+        <nav class="navegation">
             <a href="?class=Crud&function=readImagenes">Home</a>
-            <a href="#">Log Out</a>
             <a href="?class=Views&function=viewAddImagenes">Agregar Imagen</a>
+            <a href="#">Log Out</a>
         </nav>
 
 
