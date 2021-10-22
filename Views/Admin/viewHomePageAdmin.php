c22f8a111fc8fe93dd224f2c2a06d4646546643d
Hnry1304 <henryjvp24@hotmail.com>
2021-10-21 23:35:58 -0500

    Agregamos un article que guarda todos lo datos htm y php

--- Views/Admin/viewHomePageAdmin.php
+++ Views/Admin/viewHomePageAdmin.php
@@ -1,11 +1,11 @@
 <?php require_once 'Views/Assets/Layouts/Admin/header.php'?>
-
-<?php  foreach($information as $information):?>
-    <div class="imagenes">
-        <img src="/imagenes/<?= $information['URL_IMAGEN']; ?>" alt="">
-    </div>
-<?php endforeach?>
-
-
-
+    <article class="box">
+        <?php  foreach($information as $information):?>
+            <div class="cuadro">
+                <a href="#">
+                    <img src="/imagenes/<?= $information['URL_IMAGEN']; ?>" alt="" class="imagenes">
+                </a>
+            </div>
+        <?php endforeach?>
+    </article>
 <?php require_once 'Views/Assets/Layouts/Admin/footer.php'?>
