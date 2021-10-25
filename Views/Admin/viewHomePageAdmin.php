79b89b4688fcdbc1d603a2a50e21d5e4b4bc5c76
Hnry1304 <henryjvp24@hotmail.com>
2021-10-25 12:48:54 -0500

    Agregmos la vistaPaginacion.php

--- Views/Admin/viewHomePageAdmin.php
+++ Views/Admin/viewHomePageAdmin.php
@@ -1,11 +1,15 @@
 <?php require_once 'Views/Assets/Layouts/Admin/header.php'?>
-    <article class="box">
-        <?php  foreach($information as $information):?>
-            <div class="cuadro">
-                <a href="#">
-                    <img src="/imagenes/<?= $information['URL_IMAGEN']; ?>" alt="" class="imagenes">
-                </a>
-            </div>
-        <?php endforeach?>
-    </article>
+    <section class="content">
+        <article class="box">
+            <?php  foreach($information as $information):?>
+                <div class="cuadro">
+                    <a href="#">
+                        <img src="/imagenes/<?= $information['URL_IMAGEN']; ?>" alt="" class="imagenes">
+                    </a>
+                </div>
+            <?php endforeach?>
+        </article>
+
+        <?php require_once 'Views/Admin/vistaPaginacion.php'?>
+    </section>
 <?php require_once 'Views/Assets/Layouts/Admin/footer.php'?>
