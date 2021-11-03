0b95252686ce126a443afcb43ec891a6623833f8
Hnry1304 <henryjvp24@hotmail.com>
2021-11-03 13:13:02 -0600

    Agregamos la calse tittle

--- Views/Assets/Layouts/Clients/header.php
+++ Views/Assets/Layouts/Clients/header.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Views/Assets/Css/Clientes/header.css">
     <title>Document</title>
 </head>
 <body>
     <div class ="container">
         <nav>
-            <a href="#">Title</a>
-            <a href="?class=views&function=viewLoginPage">Login</a>
+            <a href="#" class="title">Title</a>
+            <a href="?class=views&function=viewLoginPage" class="login">Login</a>
         </nav>    	
