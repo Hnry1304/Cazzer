24c94c76c657bf53a0ce4dc49777f4909e8486a0
Hnry1304 <henryjvp24@hotmail.com>
2021-11-03 18:12:23 -0600

    Agregamos los includes a Models/Cliente/Crud y Paginacion, LLamamos a la clase

--- Controllers/ViewsController.php
+++ Controllers/ViewsController.php
@@ -1,8 +1,15 @@
 <?php
+    require_once 'Models/Cliente/Crud.php';
+    require_once 'Models/Cliente/paginacionModel.php';
     class ViewsController{
 
         // Vista Pagina Principal
             public function vistaHomePage(){
+                //LLamaremos la paginacion.php, ahi tomaremos postPorPagina e inicio...
+                require_once 'Paginacion/Cliente/paginacion.php';
+
+                $read_imagenes = new Crud;
+                $information = $read_imagenes->read($inicio, $postPorPagina);
                 require_once 'Views/HomePage/homePage.php';
             }
         
@@ -10,8 +17,7 @@
             public function viewLoginPage(){
                 require_once 'Views/Login/viewLoginPage.php';
             }
-
-        
+ 
         // Vista Agregar Imagen Admi
             public function viewAddImagenes(){
                 require_once 'Views/Admin/viewAddImagenes.php';
