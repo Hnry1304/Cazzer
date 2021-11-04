fb4026d9e946865248e6b56a6192556d99bddf6e
Hnry1304 <henryjvp24@hotmail.com>
2021-11-03 18:20:52 -0600

    Agredimos codigo para mostrar las imagenes

--- Views/HomePage/homePage.php
+++ Views/HomePage/homePage.php
@@ -1,3 +1,16 @@
 <?php require_once 'Views/Assets/Layouts/Clients/header.php'?>
+    
+    <section class="content">
+        <article class="box">
+            <?php  foreach($information as $information):?>
+                <div class="cuadro">
+                    <a href="#">
+                        <img src="/imagenes/<?= $information['URL_IMAGEN']; ?>" alt="" class="imagenes">
+                    </a>
+                </div>
+            <?php endforeach?>
+        </article>
 
+        <?php require_once 'Views/HomePage/vistaPaginacion.php'?>
+    </section>
 <?php require_once 'Views/Assets/Layouts/Clients/footer.php'?>
