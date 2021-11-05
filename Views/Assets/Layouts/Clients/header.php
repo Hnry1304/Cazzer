7113e0d1ee43b65c2f1095000c5943c82188465d
Hnry1304 <henryjvp24@hotmail.com>
2021-11-04 22:35:53 -0600

    Agregamos una nueva direccion Css, agregamos un isset que nos direccionara a la pagina en la que estabamos

--- Views/Assets/Layouts/Clients/header.php
+++ Views/Assets/Layouts/Clients/header.php
@@ -8,10 +8,17 @@
     <title>Document</title>
     <link rel="stylesheet" href="Views/Assets/Css/Clientes/imagenes.css">
     <link rel="stylesheet" href="Views/Assets/Css/Clientes/paginacion.css">
+    <link rel="stylesheet" href="Views/Assets/Css/Clientes/vistaPrevia.css">
+
 </head>
 <body>
     <div class ="container">
         <nav>
-            <a href="?class=Views&function=vistaHomePage&pagina=1" class="title">Title</a>
+            <?php if(isset($_GET['pagina'])): ?>
+                <a href="?class=Views&function=vistaHomePage&pagina=<?= $_GET['pagina']?>" class="title">Title</a>
+            
+            <?php else: ?>
+                <a href="?class=Views&function=vistaHomePage&pagina=1" class="title">Title</a>
+            <?php endif?>
             <a href="?class=views&function=viewLoginPage" class="login">Login</a>
         </nav>    	
