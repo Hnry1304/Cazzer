32386dc5f990e3d547390cb07ec0b4bbc792f86a
Hnry1304 <henryjvp24@hotmail.com>
2021-11-05 00:08:06 -0600

    Primer avance con la paginacion

--- Views/HomePage/vistaPaginacion.php
+++ Views/HomePage/vistaPaginacion.php
@@ -5,18 +5,36 @@
         <a href="?class=Views&function=vistaHomePage&pagina=<?= $pagina -1 ?>">Back</a>
     <?php endif ?>
 
-    <?php for($i=0; $i<$numero_paginacion; $i++): ?>
-        <?php if($pagina == $i+1):?>
-            <a href="?class=Views&function=vistaHomePage&pagina=<?=$i+1 ?>" class="posicion">
-                <?= $i+1 ?>
-            </a>
-        <?php else: ?>
-            <a href="?class=Views&function=vistaHomePage&pagina=<?=$i+1 ?>">
-                <?= $i+1 ?>
-            </a>
-        <?php endif ?>
+    <?php if($pagina <6):?>
 
-    <?php endfor ?>
+        <?php for($i=0; $i<5; $i++): ?>
+            <?php if($pagina == $i+1):?>
+                <a href="?class=Views&function=vistaHomePage&pagina=<?=$i+1 ?>" class="posicion">
+                    <?= $i+1 ?>
+                </a>
+            <?php else: ?>
+                <a href="?class=Views&function=vistaHomePage&pagina=<?=$i+1 ?>">
+                    <?= $i+1 ?>
+                </a>
+            <?php endif ?>
+
+        <?php endfor ?>
+    
+    <?php else:?>
+        <?php for($i=5; $i<$numero_paginacion; $i++): ?>
+            <?php if($pagina == $i+1):?>
+                <a href="?class=Views&function=vistaHomePage&pagina=<?=$i+1 ?>" class="posicion">
+                    <?= $i+1 ?>
+                </a>
+            <?php else: ?>
+                <a href="?class=Views&function=vistaHomePage&pagina=<?=$i+1 ?>">
+                    <?= $i+1 ?>
+                </a>
+            <?php endif ?>
+
+        <?php endfor ?>
+    <?php endif ?>
+    
 
     <?php if($pagina == $numero_paginacion):?>
         <a href="?class=Views&function=vistaHomePage&pagina=<?=$pagina +1 ?>" class="disiable">Next</a>
