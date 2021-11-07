096cfca78d3a349a1ed23b3554ac40887fb15cfd
Hnry1304 <henryjvp24@hotmail.com>
2021-11-07 05:18:29 -0600

    Agregamos mas 2 imagenes de playeras, uno blanco y uno gris

--- Views/HomePage/Options/vistaImagen.php
+++ Views/HomePage/Options/vistaImagen.php
@@ -1,7 +1,11 @@
 <?php require_once 'Views/Assets/Layouts/Clients/header.php' ?>
     <div class="container">
         <div class="imagenes">
-            <img src="/imagenes/playera_negra.png" alt="playera_negra" class="shirt">
+
+            <img src="/imagenes/playera_negra.png" alt="playera_negra" class="shirt" id="playera_negra">
+            <img src="/imagenes/Playera_blanca.PNG" alt="playera_blanca" class="shirt" id="playera_blanca">
+            <img src="/imagenes/playera_gris.png" alt="playera_gris" class="shirt" id="playera_gris">
+            
             <img src="/imagenes/<?= $information['URL_IMAGEN']; ?>" alt="" class="img">
 
         </div>
@@ -9,3 +13,4 @@
     </div>
 
 <?php require_once 'Views/Assets/Layouts/Clients/footer.php' ?>
+
