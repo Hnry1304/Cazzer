4d947c9ee6fdad4e446f1fdf8dc4305ca0f45b98
Hnry1304 <henryjvp24@hotmail.com>
2021-12-05 10:49:57 -0600

    Creamos arhiivo de validaciones para los campos Gorra,Playera y Taza

---
+++ Validations/Formulario_producto/playera.php
@@ -0,0 +1,25 @@
+<?php
+    $sexShirt = $_POST['sexo'];
+    $colorShirt = $_POST['color'];
+    $tallShirt = $_POST['talla'];
+    $cantidad_playera = $_POST['cantidad'];
+
+    if(empty($sexShirt)|| empty($colorShirt)|| empty($tallShirt)){
+        header('Location: index.php?class=Views&function=vistaHomePage&pagina=1');
+    }
+
+    $sexShirt = filter_var($sexShirt, FILTER_SANITIZE_STRING);
+    $colorShirt = filter_var($colorShirt, FILTER_SANITIZE_STRING);
+    $tallShirt = filter_var($tallShirt, FILTER_SANITIZE_STRING);
+
+    if($sexShirt != 'Caballero' && $sexShirt != 'Dama' && $sexShirt != 'Nino'){
+        header('Location: index.php?class=Views&function=vistaHomePage&pagina=1');
+    }elseif($colorShirt != 'Negro' && $colorShirt != 'Blanco' && $colorShirt != 'Gris'){
+        header('Location: index.php?class=Views&function=vistaHomePage&pagina=1');
+    }elseif($tallShirt != 'Mediana' && $tallShirt != 'Grande' && $tallShirt != 'Chica'){
+        header('Location: index.php?class=Views&function=vistaHomePage&pagina=1');
+    }elseif($cantidad_playera < 1 ){
+        header('Location: index.php?class=Views&function=vistaHomePage&pagina=1');
+    }else{
+        require_once 'Views/HomePage/Options/formulario_compraFinal.php';
+    }
