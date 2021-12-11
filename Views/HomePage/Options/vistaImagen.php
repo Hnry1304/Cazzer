644ae0d38aaf8880f0371de3305ab817c322b6bd
Hnry1304 <henryjvp24@hotmail.com>
2021-12-10 23:10:29 -0600

    Agregamos id a los divs que contienen las imagenes de los productos

--- Views/HomePage/Options/vistaImagen.php
+++ Views/HomePage/Options/vistaImagen.php
@@ -2,37 +2,37 @@
     <div class="container">
         <div class="imagenes">
 
-            <div class="playeraCaballero">
+            <div class="playeraCaballero" id="playeraCaballero">
                 <img src="/imagenes/Ropa/playera_negra_hombre.png"  alt="playera_negra"     class="shirt" id="playera_negra_caballero">
                 <img src="/imagenes/Ropa/playera_blanca_hombre.png" alt="playera_blanca"    class="shirt" id="playera_blanca_caballero">
                 <img src="/imagenes/Ropa/playera_gris_hombre.png"   alt="playera_gris"      class="shirt" id="playera_gris_caballero">
 
             </div>
 
-            <div class="playeraDama">
+            <div class="playeraDama" id="playeraDama">
                 <img src="/imagenes/Ropa/playera_negra_mujer.png"   alt="playera_negra"     class="shirt" id="playera_negra_dama">
                 <img src="/imagenes/Ropa/playera_blanca_mujer.png"  alt="playera_blanca"    class="shirt" id="playera_blanca_dama">
                 <img src="/imagenes/Ropa/playera_gris_mujer.png"    alt="playera_gris"      class="shirt" id="playera_gris_dama">
             </div>
 
-            <div class="playeraNinos">
+            <div class="playeraNinos" id="playeraNinos">
                 <img src="/imagenes/Ropa/playera_nino_negro.png"    alt="playera_negra"     class="shirt" id="playera_negra_kids">
                 <img src="/imagenes/Ropa/playera_nino_blanco.png"   alt="playera_blanca"    class="shirt" id="playera_blanca_kids">
                 <img src="/imagenes/Ropa/playera_nino_gris.png"     alt="playera_gris"      class="shirt" id="playera_gris_kids">
             </div>
 
-            <div class="sudaderasUnisex">
+            <div class="sudaderasUnisex" id="sudaderasUnisex">
                 <img src="/imagenes/Ropa/sudadera_negra.png"    alt="sudadera_negra"     class="shirt" id="sudadera_negra">
                 <img src="/imagenes/Ropa/sudadera_blanca.png"   alt="sudadera_blanca"    class="shirt" id="sudadera_blanca">
                 <img src="/imagenes/Ropa/sudadera_gris.png"     alt="sudadera_gris"      class="shirt" id="sudadera_gris">
             </div>
 
-            <div class="TazasBlanks">
+            <div class="TazasBlanks" id="TazaBlanks">
                 <img src="/imagenes/Tazas/tazaBlanca.png" alt="taza" class="taza" id="taza">
 
             </div>
 
-            <div class="GorrasTrucker">
+            <div class="GorrasTrucker" id="GorrasTrucker">
                 <img src="/imagenes/Gorras/gorra_negra.png"   alt="gorra_negra"     class="caps"     id="gorra_negra">
                 <img src="/imagenes/Gorras/gorra_blanco.png"  alt="gorra_blanca"    class="caps"     id="gorra_blanca">
                 <img src="/imagenes/Gorras/gorra_azul.png"    alt="gorra_azul"      class="caps"     id="gorra_azul">
