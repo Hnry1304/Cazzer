19312acfa75c7527721d527b226279183a121e91
Hnry1304 <henryjvp24@hotmail.com>
2021-12-19 01:15:58 -0600

    Agregamos y modificamos algunos Id del formulario Tazas

--- Views/HomePage/Options/opcion_tazas.php
+++ Views/HomePage/Options/opcion_tazas.php
@@ -2,12 +2,12 @@
     <!-- COLOR DEL PRODUCTO  -->
     <label for="Color" >
         <P>Color Producto</P>
-        <input type="radio" name="color" value="Blanco"  checked> Blanco
+        <input type="radio" name="color" value="Blanco" id="tazaBlanco" checked> Blanco
     </label>
     <br>
     <!-- CANTIDAD PRODUCTO-->
     <p>Cantidad</p>
-    <input type="number" name="Cantidad" >
+    <input type="number" name="Cantidad" value="1" id="cantidadCompra">
 
     <input type="submit" name="submit" value="Comprar Taza">
 </form>
