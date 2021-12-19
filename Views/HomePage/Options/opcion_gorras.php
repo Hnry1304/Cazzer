71b4bbed007997717805304fb3d8b6622fdc0a45
Hnry1304 <henryjvp24@hotmail.com>
2021-12-19 01:14:02 -0600

    Agregamos mas colores de Gorras

--- Views/HomePage/Options/opcion_gorras.php
+++ Views/HomePage/Options/opcion_gorras.php
@@ -1,22 +1,25 @@
 <form action="?class=Views&function=producto" method="POST">    
     <label for="tamano">
         <p>Tamano</p>
-        <input type="radio" name="talla" value="Adulto" checked> Adulto
-        <input type="radio" name="talla" value="Nino"> Nino
+        <input type="radio" name="talla" value="Adulto" id="gorraAdulto" checked> Adulto
+        <input type="radio" name="talla" value="Nino" id="gorraNino"> Nino
     </label>
 
     <!-- COLOR DEL PRODUCTO  -->
     <label for="Color">
         <P>Color Producto</P>
-        <input type="radio" name="color" value="Negro" checked> Negro
-        <input type="radio" name="color" value="Blanco" > Blanco
-        <input type="radio" name="color" value="Gris" > Gris
+        <input type="radio" name="color" value="Negro" id="gorraNegro" checked> Negro
+        <input type="radio" name="color" value="Blanco" id="gorraBlanco"> Blanco
+        <input type="radio" name="color" value="Azul" id="gorraAzul"> Azul
+        <input type="radio" name="color" value="Naranja" id="gorraNaranja">Naranja
+        <input type="radio" name="color" value="Rojo" id="gorraRojo">Rojo
+        <input type="radio" name="color" value="Rosa" id="gorraRosa">Rosa
 
     </label>
     <br>
     <!-- CANTIDAD PRODUCTO-->
     <p>Cantidad</p>
-    <input type="number" name="Cantidad" >
+    <input type="number" name="Cantidad" value="1" id="cantidadCompra">
 
-    <input type="submit" name="submit" value="Comprar Gorra">
+    <input type="submit" name="submit" value="Comprar Gorra" id="Comprar">
 </form>
