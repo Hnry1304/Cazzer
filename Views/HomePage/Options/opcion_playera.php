05a1519b6c6c495aeee703d7d28c516e0d9819b1
Hnry1304 <henryjvp24@hotmail.com>
2021-12-19 01:14:52 -0600

    Agremos id a toda la opcion de formulario Playeras

--- Views/HomePage/Options/opcion_playera.php
+++ Views/HomePage/Options/opcion_playera.php
@@ -2,18 +2,18 @@
     <!-- SEXO PLAYERA-->
     <label for="Sexo">
         <p>Sexo</p>
-        <input type="radio" name="sexo" value="Caballero"  checked> Caballero
-        <input type="radio" name="sexo" value="Dama"> Dama
-        <input type="radio" name="sexo" value="Nino"> Nino
+        <input type="radio" name="sexo" value="Caballero"  id="OptionsplayeraCaballero" checked> Caballero
+        <input type="radio" name="sexo" value="Dama" id="OptionsplayeraDama"> Dama
+        <input type="radio" name="sexo" value="Nino" id="OptionsplayeraNino"> Nino
     </label>
     <br>
 
     <!-- COLOR DEL PRODUCTO  -->
     <label for="Color">
         <P>Color Producto</P>
-        <input type="radio" name="color" value="Negro"  id="Negro" checked> Negro
-        <input type="radio" name="color" value="Blanco" id="Blanco"> Blanco
-        <input type="radio" name="color" value="Gris"   id="Gris"> Gris
+        <input type="radio" name="color" value="Negro"  id="playeraNegra" checked> Negro
+        <input type="radio" name="color" value="Blanco" id="playeraBlanca"> Blanco
+        <input type="radio" name="color" value="Gris"   id="playeraGris"> Gris
 
     </label>
     <br>
@@ -21,16 +21,16 @@
     <!-- TALLA DE LA PRENDA-->
     <label for="Talla" >
         <p>Talla</p>
-        <input type="radio" name="talla" value="Grande" > Grande
-        <input type="radio" name="talla" value="Mediana" checked> Mediana
-        <input type="radio" name="talla" value="Chica" > Chica
+        <input type="radio" name="talla" value="Grande" id="playeraGrande"> Grande
+        <input type="radio" name="talla" value="Mediana" id="playeraMediana" checked> Mediana
+        <input type="radio" name="talla" value="Chica" id="playeraChica"> Chica
     </label>
     <br>
 
     
     <!-- CANTIDAD PRODUCTO-->
     <p>Cantidad</p>
-    <input type="number" name="Cantidad" >
+    <input type="number" name="Cantidad" value="1" id="cantidadCompra">
 
     <input type="submit" name="submit" value="Comprar Playera">
 </form>
