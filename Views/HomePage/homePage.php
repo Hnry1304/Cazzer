ddc20e855860d4b69f11b9a58a67c098c75b5faf
Hnry1304 <henryjvp24@hotmail.com>
2022-01-15 15:37:41 -0600

    Quitamos exceso de codigo

--- Views/HomePage/homePage.php
+++ Views/HomePage/homePage.php
@@ -14,29 +14,4 @@
         <?php require_once 'Views/HomePage/vistaPaginacion.php'?>
     </section> -->
 
-    <section class="content">
-        <div class="opcion_playeras">
-            <a href="#">Playeras</a>
-        </div>
-
-        <div class="opcion_sudaderas">
-            <a href="#">Sudaderas</a>
-        </div>
-        
-        <div class="opcion_tazas">
-            <a href="#">Tazas</a>
-        </div>
-
-        <div class="opcion_gorras">
-            <a href="#">Gorras</a>    
-        </div>
-
-        <div class="opcion_Fundas_celular">
-            <a href="#">Fundas Celular</a>
-        </div>
-
-        <div class="opcion_Personalizado">
-            <a href="#">Personalizado</a>
-        </div>
-    </section>
 <?php require_once 'Views/Assets/Layouts/Clients/footer.php'?>
