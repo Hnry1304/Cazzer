04d811056508af372eb0d47869515b158aca1180
Hnry1304 <henryjvp24@hotmail.com>
2022-01-18 13:40:31 -0600

    quitamos las imagenes de los producto, solo quedara lo esencial para la pagina Principal...

--- Views/HomePage/homePage.php
+++ Views/HomePage/homePage.php
@@ -1,36 +1,28 @@
 <?php require_once 'Views/Assets/Layouts/Clients/header.php'?>
-   <!--
-    <section class="content">
-        <article class="box">
-            <?php  foreach($information as $information):?>
-                <div class="cuadro">
-                    <a href="/Views/vistaImagen&id=<?= $information['ID'] ?>&pagina=<?= $_GET['pagina'] ?>">
-                        <img src="/imagenes/<?= $information['URL_IMAGEN']; ?>" alt="" class="imagenes">
-                    </a>
-                </div>
-            <?php endforeach?>
-        </article>
-
-        <?php require_once 'Views/HomePage/vistaPaginacion.php'?>
-    </section> -->
-
     <section class="contenido_principal">
         <div class="imagen_publicidad">
             <a href="#">
                <img src="/imagenes/Taza.jpg" alt="">
             </a>
         </div>
+        
+        <div class="buscar">
+            <input type="search">
+            <button>Search</button>
+        </div>
+
         <div class="categories">
             <ul>
-                <li><a href="#">Tazas</a></li>
-                <li><a href="#">Fundas Celular</a></li>
-                <li><a href="#">Playeras</a></li>
-                <li><a href="#">Sudaderas</a></li>
-                <li><a href="#">Gorras</a></li>
-                <li><a href="#">Personalizacion</a></li>               
+                <li><a href="/Views/viewProduct&pagina=1&producto=taza">Tazas</a></li>
+                <li><a href="/Views/viewProduct&pagina=1&producto=funda">Fundas Celular</a></li>
+                <li><a href="/Views/viewProduct&pagina=1&producto=playera">Playeras</a></li>
+                <li><a href="/Views/viewProduct&pagina=1&producto=sudadera">Sudaderas</a></li>
+                <li><a href="/Views/viewProduct&pagina=1&producto=gorra">Gorras</a></li>
+                <li><a href="/Views/viewProduct&pagina=1&producto=personalizacion">Personalizacion</a></li>               
             </ul>
         </div>
 
     </section>
+    
 
 <?php require_once 'Views/Assets/Layouts/Clients/footer.php'?>
