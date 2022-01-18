ad7c4ea60a8f9d902b3a97906f34e59a93da1cc1
Hnry1304 <henryjvp24@hotmail.com>
2022-01-18 10:41:51 -0600

    Agregamos div de publicidad,6 divs con link directo a vista productos

--- Views/HomePage/homePage.php
+++ Views/HomePage/homePage.php
@@ -1,5 +1,5 @@
 <?php require_once 'Views/Assets/Layouts/Clients/header.php'?>
-<!--     
+   <!--
     <section class="content">
         <article class="box">
             <?php  foreach($information as $information):?>
@@ -13,5 +13,24 @@
 
         <?php require_once 'Views/HomePage/vistaPaginacion.php'?>
     </section> -->
+
+    <section class="contenido_principal">
+        <div class="imagen_publicidad">
+            <a href="#">
+               <img src="/imagenes/Taza.jpg" alt="">
+            </a>
+        </div>
+        <div class="categories">
+            <ul>
+                <li><a href="#">Tazas</a></li>
+                <li><a href="#">Fundas Celular</a></li>
+                <li><a href="#">Playeras</a></li>
+                <li><a href="#">Sudaderas</a></li>
+                <li><a href="#">Gorras</a></li>
+                <li><a href="#">Personalizacion</a></li>               
+            </ul>
+        </div>
+
+    </section>
 
 <?php require_once 'Views/Assets/Layouts/Clients/footer.php'?>
